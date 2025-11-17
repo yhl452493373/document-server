434a751575523bcd60248c7c280c65d98339ca4e
yanghuanglin <yanghuanglin@qq.com>
2025-11-17 12:39:04 +0800

    增加镜像创建人

--- document-docker/build.sh
+++ document-docker/build.sh
@@ -18,14 +18,14 @@ cp ../document-server/jodconverter-document-server/target/application.yml app/
 
 #amd64
 echo 开始构建amd64镜像...
-docker buildx build --platform linux/amd64 -t document-server:1.0 ./
+docker buildx build --platform linux/amd64 -t yhl452493373/document-server:1.0 ./
 rm -rf document-server-1.0.tar.gz
 docker save document-server:1.0 | gzip > document-server-1.0.tar.gz
 echo amd64镜像构建成功.
 
 #arm64
 echo 开始构建arm64镜像...
-docker buildx build --platform linux/arm64 -t document-server:1.0.arm64 ./
+docker buildx build --platform linux/arm64 -t yhl452493373/document-server:1.0.arm64 ./
 rm -rf document-server-1.0.arm64.tar.gz
 docker save document-server:1.0.arm64 | gzip > document-server-1.0.arm64.tar.gz
 echo arm64镜像构建成功.
