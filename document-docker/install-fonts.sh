461564a61567baaed3348aebcf5eb010a576c0c4
杨黄林 <yhl452493373@gmail.com>
2026-01-08 15:08:55 +0800

    增加CESI、国标（GB）、WPS字体

--- document-docker/install-fonts.sh
+++ document-docker/install-fonts.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo installing fonts
 sudo mkdir -p /usr/share/fonts/truetype
-sudo cp -r ./fonts/* /usr/share/fonts/truetype/
+sudo mkdir -p /usr/share/fonts/cesi
+sudo mkdir -p /usr/share/fonts/gb
+sudo mkdir -p /usr/share/fonts/wps-office
+sudo cp -r ./fonts/truetype/* /usr/share/fonts/truetype/
+sudo cp -r ./fonts/cesi/* /usr/share/fonts/cesi/
+sudo cp -r ./fonts/gb/* /usr/share/fonts/gb/
+sudo cp -r ./fonts/wps-office/* /usr/share/fonts/wps-office/
 sudo fc-cache -fv
 echo fonts installed
